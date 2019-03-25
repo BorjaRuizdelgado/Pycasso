7ad6983d8ed330268d0d07a9946d0cc1f1d6c6ca
Borja Ruizdelgado <borjafruizdelgado@gmail.com>
2019-03-25 16:44:39 +0300

    More shit bru

--- pycasso.py
+++ pycasso.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw, ImageChops
+from PIL import Image, ImageDraw
 from random import randint
 import argparse
 import sys
@@ -25,6 +25,36 @@ class Point:
         self.x = x
         self.y = y
 
+class Picture:
+    def __init__(self, size, numberdots):
+        self.size = size
+        width, height = size
+        self.points = [Point(width,height) for i in range(numberdots)]
+    
+    def composeImage(self):
+        newImage = Image.new("RGB",self.size,(255,255,255))
+        canvas = ImageDraw.Draw(newImage)
+        for point in self.points:
+                canvas.ellipse([point.x-point.radius,point.y-point.radius,point.x+point.radius,point.y+point.radius],outline=(point.r,point.g,point.b),fill=(point.r,point.g,point.b))
+        return newImage
+
+    #from STACK OVERFLOW need a new fitness function
+    def fitness(self, image2):
+        #Convert Image types to numpy arrays
+        i1 = numpy.array(self.composeImage(),numpy.int16)
+        i2 = numpy.array(image2,numpy.int16)
+        dif = numpy.sum(numpy.abs(i1-i2))
+        return (dif / 255.0 * 100) / i1.size
+
+
+class Population:
+    def __init__(self, size, numberdots, populationSize):
+        self.pictures = [Picture(size, numberdots) for i in range(populationSize)]
+    
+    def crossover(self):
+        print("Crossover needs to be implemented")
+
+
 
 def print_same_line(text):
     sys.stdout.write('\r')
@@ -32,45 +62,29 @@ def print_same_line(text):
     sys.stdout.write(text)
     sys.stdout.flush()
 
-def composeImage(points, size):
-    newImage = Image.new("RGB",size,(255,255,255))
-    canvas = ImageDraw.Draw(newImage)
-    for point in points:
-            canvas.ellipse([point.x-point.radius,point.y-point.radius,point.x+point.radius,point.y+point.radius],outline=(point.r,point.g,point.b),fill=(point.r,point.g,point.b))
-    return newImage
 
 
-#from STACK OVERFLOW need a new fitness function
-def fitness(image1, image2):
-     #Convert Image types to numpy arrays
-    i1 = numpy.array(image1,numpy.int16)
-    i2 = numpy.array(image2,numpy.int16)
-    dif = numpy.sum(numpy.abs(i1-i2))
-    return (dif / 255.0 * 100) / i1.size
-
-
-def generateImage(imageTarget, generations, numberdots):
+def generateImage(imageTarget, generations, numberdots, populationSize):
     generation = 0
-    width, height = imageTarget.size
-    allPoints = [Point(width,height) for i in range(numberdots)]
+    population = Population(imageTarget.size,numberdots,populationSize)
 
     while (generation != generations):
         print_same_line("Generation number: " + str(generation))
         generation += 1
 
-        newImage = composeImage(allPoints, imageTarget.size)
-        newImage.show()
+    
         
 
     print("\n")
         
-def pycasso(filePath, generations,numberdots):
+def pycasso(args):
 
     try:
-        imageTarget = Image.open(filePath)
+        print(args)
+        imageTarget = Image.open(args.path)
         imageTarget.show()
-        print('The image will be painted with ' + str(numberdots) + ' dots.\n')
-        generateImage(imageTarget,generations,numberdots)
+        print('The image will be painted with ' + str(args.numberdots) + ' dots.\n')
+        generateImage(imageTarget, args.numbergenerations, args.numberdots, args.populationsize)
     except IOError:
         print("Couldn't open the file")
         exit()
@@ -80,8 +94,9 @@ def main():
     parser.add_argument('--path', default=None, required=True, help='Path to find the image')
     parser.add_argument('--numbergenerations', default=1, required=False,help='Number of generations the program will make')
     parser.add_argument('--numberdots', default=1000,required=False, help='Number of dots to generate the image')
+    parser.add_argument('--populationsize', default=100,required=False, help='Size of the population')
     args = parser.parse_args()
-    pycasso(args.path, args.numbergenerations,args.numberdots)
+    pycasso(args)
 
 
 
